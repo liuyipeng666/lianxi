4cd1d2d5e0a09e1fdd5ba446a8c70cf3127cedd0
liuyipeng666 <2969166951@qq.com>
2021-03-23 08:58:48 +0800

    第二次提交

--- app/src/main/java/com/example/yuancheng/MainActivity.java
+++ app/src/main/java/com/example/yuancheng/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.yuancheng;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private TextView t1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        
+        t1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "你真牛", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void initView() {
+        t1 = (TextView) findViewById(R.id.t1);
     }
 }
